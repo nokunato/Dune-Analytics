61ee776b83db58ef9d086402ad7a1df8151d3dec
nokunato <55089026+nokunato@users.noreply.github.com>
2022-05-18 10:45:33 +0100

    Add queries for sETH on Polynomial

---
+++ sETH Vault queries/sETH COvered Call Vault transactions per day.sql
@@ -0,0 +1,16 @@
+-- number of deposit transactions per day
+with optimism as (
+select * from optimism."transactions"
+),
+ser as (
+select generate_series((SELECT min("block_time") FROM optimism)::timestamptz, now(), '1 hour')
+)
+select 
+    date_trunc('day', "block_time") as day
+    , count(*)
+from optimism."transactions" o 
+left join ser s on generate_series = o.block_time
+where "to" = '\x331Cf6E3E59B18a8bc776A0F652aF9E2b42781c5'
+and success = 'true'
+and "block_time" > now() - interval '{{Time frame}}'
+group by 1
