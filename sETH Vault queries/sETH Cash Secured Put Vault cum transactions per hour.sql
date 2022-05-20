1f44f92ab4687422b712e498e199d5d05ca9440c
nokunato <55089026+nokunato@users.noreply.github.com>
2022-05-20 10:30:38 +0100

    Add queries for sETH on Polynomial

---
+++ sETH Vault queries/sETH Cash Secured Put Vault cum transactions per hour.sql
@@ -0,0 +1,10 @@
+
+-- cummulative transactions per hour
+select 
+    date_trunc('hour', "block_time") as hour
+    , count(*) over (order by block_time)
+from optimism."transactions"
+where "to" = '\xFa923AA6b4DF5bea456DF37FA044B37F0FDDCdb4'
+and success = 'true'
+and "block_time" > now() - interval '{{Time frame}}'
+-- group by 1
